8c0fd75f1ae475685c2338d1e9ca60153aed878d
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-05-24 11:24:31 +0530

    initial commmit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100] 
+  end
+
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+end
