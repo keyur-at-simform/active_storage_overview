e5c8254d4c114ab506e66c8df3e205913e3619c8
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-05-31 14:22:26 +0530

    fix: TE5-T109 changed db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_091249) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_064051) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,12 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_091249) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "meetings", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
